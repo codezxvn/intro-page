66c0461420f86bd06ca813e3c7f7320da725d906
cuongpq1000vn <phamquoccuongv@gmail.com>
2024-04-19 16:32:48 +0700

    udpate new code

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -3,3 +3,4 @@ export { default as Footer } from "./footer";
 export { default as Header } from "./header";
 export { default as SolutionOrganisms } from "./solution-content";
 export { default as Teams } from "./teams";
+export { default as Portfolio } from "./portfolio";
