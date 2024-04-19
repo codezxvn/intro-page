cf1ad9190268c0219609afa17484c55ffee18b81
cuongpq1000vn <phamquoccuongv@gmail.com>
2024-04-19 11:26:16 +0700

    fix responsive for footer and solution content

--- src/components/molecures/card.tsx
+++ src/components/molecures/card.tsx
@@ -1,4 +1,4 @@
-
+import { IoIosArrowForward } from "react-icons/io";
 import styles from "./styles/ProductCard.module.css";
 import { CardProps } from "./types/solutionType";
 
@@ -21,7 +21,7 @@ export default function CardMolicule({ title, iconAPI }: CardProps) {
       <div className={styles.media}>
         <div className={styles.iconContent}>
           <Icon
-            className={`leading-6 relative text-4xl text-neutral ${styles["icon"]}`}
+            className={`leading-6 relative text-6xl text-neutral ${styles["icon"]}`}
           />
         </div>
         <div className={styles.body}>
@@ -31,6 +31,12 @@ export default function CardMolicule({ title, iconAPI }: CardProps) {
             {combineString(1)}
           </h3>
         </div>
+        <a
+          href="#"
+          className={`absolute rounded-3xl -bottom-5 w-10 h-10 right-10 text-xl bg-black items-center ${styles.btn}`}
+        >
+          <IoIosArrowForward className=" text-neutral text-3xl" />
+        </a>
       </div>
     </div>
   );
