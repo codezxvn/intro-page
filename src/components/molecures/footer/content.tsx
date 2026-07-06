01ee279d7c3a32bd7982e2cfc86c80fa95d2e5b6
Nguyen Manh Ha <manhhad32@gmail.com>
2026-07-06 13:29:18 +0700

    improve light mode

--- src/components/molecures/footer/content.tsx
+++ src/components/molecures/footer/content.tsx
@@ -16,8 +16,8 @@ export default function ContentMoliCule({ heading, content }: ContentProps) {
 
   return (
     <div>
-      <h2 className="mb-4 font-semibold uppercase text-white">{heading}</h2>
-      <ul className="text-white/80 font-medium space-y-2">
+      <h2 className="mb-4 font-semibold uppercase text-primary">{heading}</h2>
+      <ul className="dark:text-white/80 text-black/80 font-medium space-y-2">
         {content.map((item) => (
           <li
             key={item.content}
