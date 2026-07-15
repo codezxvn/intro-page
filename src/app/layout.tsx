c92f5018e7c332dbe34ec2561b7de89ec691e5f7
Nguyen Manh Ha <manhhad32@gmail.com>
2026-07-15 16:39:20 +0700

    update text color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
           <AppContext>
             <>
               <div className="app-bg-decor" />
